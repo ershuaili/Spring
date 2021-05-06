37076cebfadd5851b566de42f4ba000c906d91e9
shuai <liershuai.cn@gmail.com>
2021-05-06 22:15:50 +0800

    master

--- spring-01/src/test/java/com/example/MyTest.java
+++ spring-01/src/test/java/com/example/MyTest.java
@@ -1,11 +1,9 @@
 package com.example;
 
-import com.example.dao.UserDao;
 import com.example.dao.UserDaoImpl;
-import com.example.dao.UserDaoMysql;
-import com.example.dao.UserDaoMysqlImpl;
-import com.example.service.UserService;
+import com.example.dao.UserDaoMySqlImpl;
 import com.example.service.UserServiceImpl;
+import org.junit.jupiter.api.Test;
 
 /**
  * Created by IntelliJ IDEA.
@@ -15,12 +13,15 @@ import com.example.service.UserServiceImpl;
  * @Date 2021/05/04 下午 11:19
  */
 public class MyTest {
-    public static void main(String[] args) {
-
+    @Test
+    public void test() {
         //用户实际接触的事业务层，Dao层不会接触
-        UserServiceImpl userService = new UserServiceImpl();
-
-        userService.setUserDao(new UserDaoImpl());
-        userService.getUser();
+        UserServiceImpl service = new UserServiceImpl();
+        //调用MySQL实现
+        service.setUserDao(new UserDaoMySqlImpl());
+        service.getUser();
+        //那我们现在又想用默认去实现呢
+        service.setUserDao(new UserDaoImpl());
+        service.getUser();
     }
 }
