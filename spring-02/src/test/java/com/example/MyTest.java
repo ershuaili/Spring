b57beb0c832cd1ee8ebc5ce00735ef5b178faa3c
shuai <liershuai.cn@gmail.com>
2021-05-07 16:44:29 +0800

    master

--- spring-02/src/test/java/com/example/MyTest.java
+++ spring-02/src/test/java/com/example/MyTest.java
@@ -15,6 +15,7 @@ public class MyTest {
     public static void main(String[] args) {
         //获取Spring的上下文对象
         ApplicationContext context = new ClassPathXmlApplicationContext("ApplicationContext.xml");
+
         Hello hello =(Hello) context.getBean("hello");
         System.out.println(hello.toString());
     }
