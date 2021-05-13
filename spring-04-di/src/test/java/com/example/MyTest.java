c87b7088c7562ba642d7ff1c0840fe7ab8b2f8c9
shuai <liershuai.cn@gmail.com>
2021-05-13 20:47:09 +0800

    di

--- spring-04-di/src/test/java/com/example/MyTest.java
+++ spring-04-di/src/test/java/com/example/MyTest.java
@@ -1,6 +1,8 @@
 package com.example;
 
 import com.example.pojo.Student;
+import com.example.pojo.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -17,4 +19,11 @@ public class MyTest {
         Student student = (Student) context.getBean("student");
         System.out.println(student.toString());
     }
+    @Test
+    public void test2(){
+        ApplicationContext context = new ClassPathXmlApplicationContext("userbeans.xml");
+        User user = (User) context.getBean("user");
+        System.out.println(user);
+    }
+
 }
