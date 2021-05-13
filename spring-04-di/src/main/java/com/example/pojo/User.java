2e16fb2b00c733e42a23291c4b26914ae55de2ae
shuai <liershuai.cn@gmail.com>
2021-05-13 20:47:23 +0800

    di

---
+++ spring-04-di/src/main/java/com/example/pojo/User.java
@@ -0,0 +1,20 @@
+package com.example.pojo;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+/**
+ * Created by IntelliJ IDEA.
+ *
+ * @author 李二帅
+ * @version 1.0
+ * @Date 2021/05/13 下午 8:31
+ */
+@Getter
+@Setter
+@ToString
+public class User {
+    private String name;
+    private int age;
+}
